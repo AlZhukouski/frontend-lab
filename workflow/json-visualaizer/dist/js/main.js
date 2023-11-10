3db819966958f7e164a53a2528d87af717a09314
A.Zhukouski <al.zhukouski@gmail.com>
2023-11-10 20:14:47 +0300

    fix visualaiser task

--- workflow/json-visualaizer/dist/js/main.js
+++ workflow/json-visualaizer/dist/js/main.js
@@ -1,15 +1,14 @@
 const inputField = document.querySelector(".json__input");
 const jsonOutput = document.querySelector(".json__output");
 const button = document.querySelector(".json__button");
-
 button.addEventListener("click", () => {
     const jsonString = inputField.value;
     if (isJsonString(jsonString)) {
         const jsonObj = JSON.parse(jsonString);
         jsonOutput.innerHTML = "";
-        for (const [key, value] of Object.entries(jsonObj)) {
+        Object.entries(jsonObj).forEach(([key, value]) => {
             jsonOutput.appendChild(renderStroke(key, value));
-        }
+        });
     } else {
         jsonOutput.innerHTML = "Not Valid json string";
     }
@@ -27,36 +26,45 @@ function isJsonString(str) {
 
 function renderStroke(key, value) {
     const objStroke = document.createElement("div");
-    objStroke.className = "obj__stroke" + (typeof (value) === "object" ? " accordion" : "");
+    objStroke.className = "obj__stroke" + (typeof (value) === "object" && value != null ? " accordion" : "");
     objStroke.addEventListener("click", function () {
         event.stopPropagation();
         this.classList.toggle("open");
     });
-    objStroke.appendChild(renderObjKey(key));
-    objStroke.appendChild(renderColon());
+    objStroke.appendChild(renderPrimitiveType("span", "obj__key", key));
+    objStroke.appendChild(renderPrimitiveType("span", "obj__colon", " : "));
     objStroke.appendChild(renderObjValue(value, objStroke));
     return objStroke;
 }
 
-function renderObjKey(value) {
-    const objKey = document.createElement("span");
-    objKey.className = "obj__key";
-    objKey.textContent = value;
-    return objKey;
+function renderPrimitiveType(tagName, className, textContent) {
+    const element = document.createElement(tagName);
+    element.className = className;
+    element.textContent = textContent;
+    return element;
 }
 
-function renderColon() {
-    const colon = document.createElement("span");
-    colon.className = "obj__colon";
-    colon.textContent = " : ";
-    return colon;
+function renderObjValue(value, parentNode) {
+    switch (typeof (value)) {
+        case "object":
+            if (value == null) {
+                console.log(value == null);
+                return renderPrimitiveType("span", "obj__null", "null");
+            } else {
+                return renderObject(value, parentNode);
+            }
+        default:
+            return renderPrimitiveType("span", "obj__" + typeof (value), value);
+    }
 }
 
-function renderBracket(value) {
-    const bracket = document.createElement("span");
-    bracket.className = "obj__bracket";
-    bracket.textContent = value;
-    return bracket;
+function renderObject(obj, parentNode) {
+    parentNode.appendChild(renderPrimitiveType("span", "obj__bracket", Array.isArray(obj) ? "[" : "{"));
+    parentNode.appendChild(renderCount(obj));
+    Object.entries(obj).forEach(([key, value]) => {
+        parentNode.appendChild(renderStroke(key, value));
+    });
+    return parentNode.appendChild(renderPrimitiveType("span", "obj__bracket", Array.isArray(obj) ? "]" : "}"));
 }
 
 function renderCount(value) {
@@ -73,56 +81,4 @@ function renderCount(value) {
     }
     count.textContent = length + " " + name + (length > 1 ? "s" : "");
     return count;
-}
-
-function renderObjValue(value, parentNode) {
-    switch (typeof (value)) {
-        case "string" :
-            return renderString(value);
-            break;
-        case "number":
-            return renderNumber(value);
-            break;
-        case "boolean":
-            return renderBoolean(value);
-            break;
-        case "object":
-            return renderObject(value, parentNode);
-            break;
-    }
-}
-
-
-function renderString(value) {
-    const valueString = document.createElement("span");
-    valueString.className = "obj__string";
-    valueString.textContent = value;
-    return valueString;
-}
-
-function renderNumber(value) {
-    const valueNumber = document.createElement("span");
-    valueNumber.className = "obj__number";
-    valueNumber.textContent = value;
-    return valueNumber;
-}
-
-function renderBoolean(value) {
-    const valueBoolean = document.createElement("span");
-    valueBoolean.className = "obj__boolean";
-    valueBoolean.textContent = value;
-    return valueBoolean;
-}
-
-
-//isArray() check
-function renderObject(obj, parentNode) {
-    //objStroke.appendChild(renderBracket('['));
-    const object = document.createElement("div");
-    parentNode.appendChild(renderBracket(Array.isArray(obj) ? "[" : "{"));
-    parentNode.appendChild(renderCount(obj));
-    for (const [key, value] of Object.entries(obj)) {
-        parentNode.appendChild(renderStroke(key, value));
-    }
-    return parentNode.appendChild(renderBracket(Array.isArray(obj) ? "]" : "}"));
 }
